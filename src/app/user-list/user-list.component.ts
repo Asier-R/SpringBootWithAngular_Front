4e4c0372423ebdb06de41f61b1ec4dec93cbc31f
asier.virtualbox <asier.virtualbox@prueba.com>
2023-08-11 12:56:04 +0200

    Se crea gitignore para excluir cache

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -5,7 +5,7 @@ import { UserService } from '../service/user-service.service';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.sass']
 })
 export class UserListComponent implements OnInit {
 
